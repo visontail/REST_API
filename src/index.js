ce63e2f2bdf09c0182d7a7399c3a37f2304a9c30
visontail <the051425@gmail.com>
2023-12-06 14:47:41 +0100

    Comments

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
+const express = require('express');
 const dotenv = require('dotenv');
+const quotes = require('./data/quotes.json');
 
 dotenv.config({ path: '.env' });
-
-const express = require('express');
-
-const quotes = require('./data/quotes.json');
 
 const app = express();
 const port = process.env.PORT;
 
-
-// Route to get a random quote
+// Route to get a random quote JSON FORMAT
 app.get('/quote', (req, res) => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
-  res.json({ 'Naval Ravikant': quotes[randomIndex] });
+  res.json({ 'quote' : quotes[randomIndex] });
 });
 
-// Route to get a random quote
+// Route to get a random quote TEXT FORMAT
 app.get('/text', (req, res) => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   res.send(quotes[randomIndex]);
 });
-
 
 //  Listening Port
 app.listen(port, () => {
@@ -30,7 +26,8 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    title: "My REST API",
+    title: "REST API",
+    content: "Displays a random quote from Naval Ravikant",
   });
 });
 //  Error Handling
